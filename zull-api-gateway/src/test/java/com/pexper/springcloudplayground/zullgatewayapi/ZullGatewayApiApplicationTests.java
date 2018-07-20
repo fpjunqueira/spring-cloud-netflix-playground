98afc6e59e3a1f5bd1c3afbe13c388f0538fc881
Felipe Junqueira <felipetiuzanata@gmail.com>
2018-07-20 12:32:15 -0300

    zuul project

---
+++ zull-api-gateway/src/test/java/com/pexper/springcloudplayground/zullgatewayapi/ZullGatewayApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pexper.springcloudplayground.zullgatewayapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZullGatewayApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
