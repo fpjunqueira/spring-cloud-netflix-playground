ccf4f2df79571963c180a7352bd2a85f49bebc5a
Felipe Junqueira <felipetiuzanata@gmail.com>
2018-08-28 09:22:19 -0300

    swagger zuul module

--- zuul-gateway-api/src/main/java/com/pexper/springcloudplayground/zuulgatewayapi/ZuulGatewayApiApplication.java
+++ zuul-gateway-api/src/main/java/com/pexper/springcloudplayground/zuulgatewayapi/ZuulGatewayApiApplication.java
@@ -1,16 +1,29 @@
 package com.pexper.springcloudplayground.zuulgatewayapi;
 
+import io.swagger.models.Model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.xmlpull.v1.XmlPullParserException;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2
 public class ZuulGatewayApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayApiApplication.class, args);
 	}
+
 }
